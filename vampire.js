be343b3160163cff16688bc297d8f74b6319c7df
Safia Abdale <safia.abdale@gmail.com>
2021-12-15 06:23:21 +0000

    Wrote code to act on Vampire class

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    // climb up the tree toward root
+    while (currentVampire.creator) {
+      // while there is a boss, replace currentVampire with boss
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    // return true if this vamp is vampire's boss OR if this vamp is their bosses boss
+    if ((this === vampire.creator) || (this.offspring.includes(vampire.creator))) {
+      return true
+    }
+    return false;
   }
 
   /** Stretch **/
